e35d0c7029b6efae1df68db22e0dd709c5519071
Micael Campos <micael.campos@gmail.com>
2017-05-24 23:32:01 +0100

    Mock Utils

--- app/src/main/java/com/udacity/stockhawk/sync/QuoteSyncJob.java
+++ app/src/main/java/com/udacity/stockhawk/sync/QuoteSyncJob.java
@@ -11,6 +11,7 @@ import android.net.NetworkInfo;
 
 import com.udacity.stockhawk.data.Contract;
 import com.udacity.stockhawk.data.PrefUtils;
+import com.udacity.stockhawk.mock.MockUtils;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -25,148 +26,155 @@ import timber.log.Timber;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 import yahoofinance.histquotes.HistoricalQuote;
-import yahoofinance.histquotes.Interval;
 import yahoofinance.quotes.stock.StockQuote;
 
 public final class QuoteSyncJob {
 
-    private static final int ONE_OFF_ID = 2;
-    private static final String ACTION_DATA_UPDATED = "com.udacity.stockhawk.ACTION_DATA_UPDATED";
-    private static final int PERIOD = 300000;
-    private static final int INITIAL_BACKOFF = 10000;
-    private static final int PERIODIC_ID = 1;
-    private static final int YEARS_OF_HISTORY = 2;
+	private static final int ONE_OFF_ID = 2;
+	private static final String ACTION_DATA_UPDATED = "com.udacity.stockhawk.ACTION_DATA_UPDATED";
+	private static final int PERIOD = 300000;
+	private static final int INITIAL_BACKOFF = 10000;
+	private static final int PERIODIC_ID = 1;
+	private static final int YEARS_OF_HISTORY = 2;
 
-    private QuoteSyncJob() {
-    }
+	private QuoteSyncJob() {
+	}
 
-    static void getQuotes(Context context) {
+	static void getQuotes(Context context) {
 
-        Timber.d("Running sync job");
+		Timber.d("Running sync job");
 
-        Calendar from = Calendar.getInstance();
-        Calendar to = Calendar.getInstance();
-        from.add(Calendar.YEAR, -YEARS_OF_HISTORY);
+		Calendar from = Calendar.getInstance();
+		Calendar to = Calendar.getInstance();
+		from.add(Calendar.YEAR, -YEARS_OF_HISTORY);
 
-        try {
+		try {
 
-            Set<String> stockPref = PrefUtils.getStocks(context);
-            Set<String> stockCopy = new HashSet<>();
-            stockCopy.addAll(stockPref);
-            String[] stockArray = stockPref.toArray(new String[stockPref.size()]);
+			Set<String> stockPref = PrefUtils.getStocks(context);
+			Set<String> stockCopy = new HashSet<>();
+			stockCopy.addAll(stockPref);
+			String[] stockArray = stockPref.toArray(new String[stockPref.size()]);
 
-            Timber.d(stockCopy.toString());
+			Timber.d(stockCopy.toString());
 
-            if (stockArray.length == 0) {
-                return;
-            }
+			if (stockArray.length == 0) {
+				return;
+			}
 
-            Map<String, Stock> quotes = YahooFinance.get(stockArray);
-            Iterator<String> iterator = stockCopy.iterator();
+			Map<String, Stock> quotes = YahooFinance.get(stockArray);
+			Iterator<String> iterator = stockCopy.iterator();
 
-            Timber.d(quotes.toString());
+			Timber.d(quotes.toString());
 
-            ArrayList<ContentValues> quoteCVs = new ArrayList<>();
+			ArrayList<ContentValues> quoteCVs = new ArrayList<>();
 
-            while (iterator.hasNext()) {
-                String symbol = iterator.next();
-
-
-                Stock stock = quotes.get(symbol);
-                StockQuote quote = stock.getQuote();
-
-                float price = quote.getPrice().floatValue();
-                float change = quote.getChange().floatValue();
-                float percentChange = quote.getChangeInPercent().floatValue();
-
-                // WARNING! Don't request historical data for a stock that doesn't exist!
-                // The request will hang forever X_x
-                List<HistoricalQuote> history = stock.getHistory(from, to, Interval.WEEKLY);
-
-                StringBuilder historyBuilder = new StringBuilder();
-
-                for (HistoricalQuote it : history) {
-                    historyBuilder.append(it.getDate().getTimeInMillis());
-                    historyBuilder.append(", ");
-                    historyBuilder.append(it.getClose());
-                    historyBuilder.append("\n");
-                }
-
-                ContentValues quoteCV = new ContentValues();
-                quoteCV.put(Contract.Quote.COLUMN_SYMBOL, symbol);
-                quoteCV.put(Contract.Quote.COLUMN_PRICE, price);
-                quoteCV.put(Contract.Quote.COLUMN_PERCENTAGE_CHANGE, percentChange);
-                quoteCV.put(Contract.Quote.COLUMN_ABSOLUTE_CHANGE, change);
+			while (iterator.hasNext()) {
+				String symbol = iterator.next();
 
 
-                quoteCV.put(Contract.Quote.COLUMN_HISTORY, historyBuilder.toString());
+				Stock stock = quotes.get(symbol);
+				StockQuote quote = stock.getQuote();
 
-                quoteCVs.add(quoteCV);
+				float price = quote.getPrice().floatValue();
+				float change = quote.getChange().floatValue();
+				float percentChange = quote.getChangeInPercent().floatValue();
 
-            }
-
-            context.getContentResolver()
-                    .bulkInsert(
-                            Contract.Quote.URI,
-                            quoteCVs.toArray(new ContentValues[quoteCVs.size()]));
-
-            Intent dataUpdatedIntent = new Intent(ACTION_DATA_UPDATED);
-            context.sendBroadcast(dataUpdatedIntent);
-
-        } catch (IOException exception) {
-            Timber.e(exception, "Error fetching stock quotes");
-        }
-    }
-
-    private static void schedulePeriodic(Context context) {
-        Timber.d("Scheduling a periodic task");
+				// WARNING! Don't request historical data for a stock that doesn't exist!
+				// The request will hang forever X_x
+				// List<HistoricalQuote> history = stock.getHistory(from, to, Interval.WEEKLY);
 
 
-        JobInfo.Builder builder = new JobInfo.Builder(PERIODIC_ID, new ComponentName(context, QuoteJobService.class));
+				// Note to reviewer
+				// Due to problems with Yahoo API we have commented the line above
+				// and included this one to fetch the history from MockUtils
+				// This should be enough as to develop and review while the API is down
+				List<HistoricalQuote> history = MockUtils.getHistory();
 
 
-        builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
-                .setPeriodic(PERIOD)
-                .setBackoffCriteria(INITIAL_BACKOFF, JobInfo.BACKOFF_POLICY_EXPONENTIAL);
+				StringBuilder historyBuilder = new StringBuilder();
+
+				for (HistoricalQuote it : history) {
+					historyBuilder.append(it.getDate().getTimeInMillis());
+					historyBuilder.append(", ");
+					historyBuilder.append(it.getClose());
+					historyBuilder.append("\n");
+				}
+
+				ContentValues quoteCV = new ContentValues();
+				quoteCV.put(Contract.Quote.COLUMN_SYMBOL, symbol);
+				quoteCV.put(Contract.Quote.COLUMN_PRICE, price);
+				quoteCV.put(Contract.Quote.COLUMN_PERCENTAGE_CHANGE, percentChange);
+				quoteCV.put(Contract.Quote.COLUMN_ABSOLUTE_CHANGE, change);
 
 
-        JobScheduler scheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+				quoteCV.put(Contract.Quote.COLUMN_HISTORY, historyBuilder.toString());
 
-        scheduler.schedule(builder.build());
-    }
+				quoteCVs.add(quoteCV);
 
+			}
 
-    public static synchronized void initialize(final Context context) {
+			context.getContentResolver()
+					.bulkInsert(
+							Contract.Quote.URI,
+							quoteCVs.toArray(new ContentValues[quoteCVs.size()]));
 
-        schedulePeriodic(context);
-        syncImmediately(context);
+			Intent dataUpdatedIntent = new Intent(ACTION_DATA_UPDATED);
+			context.sendBroadcast(dataUpdatedIntent);
 
-    }
+		} catch (IOException exception) {
+			Timber.e(exception, "Error fetching stock quotes");
+		}
+	}
 
-    public static synchronized void syncImmediately(Context context) {
-
-        ConnectivityManager cm =
-                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnectedOrConnecting()) {
-            Intent nowIntent = new Intent(context, QuoteIntentService.class);
-            context.startService(nowIntent);
-        } else {
-
-            JobInfo.Builder builder = new JobInfo.Builder(ONE_OFF_ID, new ComponentName(context, QuoteJobService.class));
+	private static void schedulePeriodic(Context context) {
+		Timber.d("Scheduling a periodic task");
 
 
-            builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
-                    .setBackoffCriteria(INITIAL_BACKOFF, JobInfo.BACKOFF_POLICY_EXPONENTIAL);
+		JobInfo.Builder builder = new JobInfo.Builder(PERIODIC_ID, new ComponentName(context, QuoteJobService.class));
 
 
-            JobScheduler scheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+		builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+				.setPeriodic(PERIOD)
+				.setBackoffCriteria(INITIAL_BACKOFF, JobInfo.BACKOFF_POLICY_EXPONENTIAL);
 
-            scheduler.schedule(builder.build());
+
+		JobScheduler scheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+
+		scheduler.schedule(builder.build());
+	}
 
 
-        }
-    }
+	public static synchronized void initialize(final Context context) {
+
+		schedulePeriodic(context);
+		syncImmediately(context);
+
+	}
+
+	public static synchronized void syncImmediately(Context context) {
+
+		ConnectivityManager cm =
+				(ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+		if (networkInfo != null && networkInfo.isConnectedOrConnecting()) {
+			Intent nowIntent = new Intent(context, QuoteIntentService.class);
+			context.startService(nowIntent);
+		} else {
+
+			JobInfo.Builder builder = new JobInfo.Builder(ONE_OFF_ID, new ComponentName(context, QuoteJobService.class));
+
+
+			builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+					.setBackoffCriteria(INITIAL_BACKOFF, JobInfo.BACKOFF_POLICY_EXPONENTIAL);
+
+
+			JobScheduler scheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+
+			scheduler.schedule(builder.build());
+
+
+		}
+	}
 
 
 }
